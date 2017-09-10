86288a8395bb9ca3e9443ea84da71eb9a2075ed0
Tyler Wilding <xtvaser@gmail.com>
2017-09-09 23:04:10 -0400

    can now save things to google cloud datastore

--- backend/boilerplate.js
+++ backend/boilerplate.js
@@ -6,11 +6,15 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
     http://aws.amazon.com/apache2.0/
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
-
+require('dotenv').config()
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
 const bodyParser = require('body-parser');
+const Datastore = require('@google-cloud/datastore');
+
+// Instantiates a client
+const datastore = Datastore();
 
 const app = express();
 
@@ -41,5 +45,34 @@ app.use(express.static('../frontend'))
 
 const PORT = 8080;
 http.createServer(app).listen(PORT, function () {
-  console.log('Extension Boilerplate service running on http', PORT);
+    console.log('Extension Boilerplate service running on http', PORT);
+    const taskKey = datastore.key([
+      'Channel',
+      '1234567890'
+    ]);
+    const entity = {
+        key: taskKey,
+        data: [
+          {
+            name: 'theme',
+            value: "darkly"
+          },
+          {
+            name: 'games',
+            value: [
+                "abc1234",
+                "dfg456"
+            ]
+          }
+        ]
+    };
+
+    datastore.upsert(entity)
+        .then(() => {
+          console.log(`Task ${taskKey.id} created successfully.`);
+        })
+        .catch((err) => {
+          console.error('ERROR:', err);
+        });
+    console.log(process.env.CLOUD_ID);
 });
