29ed16b4ad6009d1cc39bb8f32d3e7bee350210f
Tyler Wilding <xtvaser@gmail.com>
2017-09-10 22:35:40 -0400

    fixed ssl generation script, although its useless

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -22,6 +22,25 @@ var objectData =
 
 var objectDataString = JSON.stringify(objectData);
 
+window.Twitch.ext.onAuthorized(function(auth) {
+  console.log('The JWT that will be passed to the EBS is', auth.token);
+  console.log('The channel ID is', auth.channelId);
+});
+
+/*
+Usually, the JWT obtained from this call is passed as a header during AJAX calls to the EBS. For example:
+
+window.Twitch.ext.onAuthorized(function(auth) {
+  $.ajax({
+    url: '/<some backend path>',
+    type: 'GET',
+    headers: {
+      'x-extension-jwt': auth.token,
+    }
+  });
+});
+*/
+
 $("#saveBtn").click(function(){
     console.log("clicked")
     $.ajax({
