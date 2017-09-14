25204422251ca6efffd154d6a022a5b8dcbe0bfe
Tyler Wilding <xtvaser@gmail.com>
2017-09-13 23:29:38 -0400

    Started work on viewer code

--- frontend/js/viewer.js
+++ frontend/js/viewer.js
@@ -1,5 +1,11 @@
 /// Javascript to render the personal bests on the channel page
 
+var games
+var title
+var theme
+var srcID
+
+var pbList = []
 
 window.Twitch.ext.onAuthorized(function(auth) {
 
@@ -16,7 +22,16 @@ window.Twitch.ext.onAuthorized(function(auth) {
         data: {},
         success: function (res) {
            console.log('Success\nResponse Code:' + res.status + '\nMessage: ' + res.message);
-           renderPersonalBests(res.data)
+           games = JSON.parse(res.data.games)
+           title = res.data.title
+           theme = res.data.theme
+           srcID = res.data.srcID
+           // First we will get all the runner's personal bests
+           $.ajax({
+               url: "https://www.speedrun.com/api/v1/users/" + srcID + "/personal-bests",
+               dataType: "jsonp",
+               jsonpCallback: "getPersonalBests"
+           });
         },
         error: function () {
             alert('Error');
@@ -24,29 +39,39 @@ window.Twitch.ext.onAuthorized(function(auth) {
     });
 });
 
-$('#game1').click(function() {
-    $('#pbList1').slideToggle('fast');
+function getPersonalBests(json) {
+
+    personalBests = json.data
+    for (let pb in personalBests) {
+        // NOTE can get rank pb.place
+        run = pb.run
+        // If this is one of the games that should be tracked
+        index = games.findIndex(x => x.id === run.game)
+        if (index > -1) {
+            pbList[run.game].push([
+                "category name" + run.category,
+                "pb - " + run.times.primary_t,
+                run.weblink,
+                run.variables // subcategories....
+            ]) // TODO change to dictionary later
+        }
+    }
+
+    // Now with all the PBs, we still need category names, and WR times
+
+
+}
+
+/// Collapse or Expand Game's Row
+$('.gameTitle').click(function(e) {
+    id = e.currentTarget.id.substring(1)
+    $('#pbRow' + id).slideToggle('fast');
 })
 
 /// Renders the Panel with the given settings
 function renderPersonalBests(data) {
 
-    console.log(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // If we find nothing, output some default stuff
 }
 
 /// Unused atm, because twitch's handler is what we actually need
