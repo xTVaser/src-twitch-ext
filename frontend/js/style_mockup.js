11adc7234519924364f22aebe2b301191dfb9741
Tyler Wilding <xtvaser@gmail.com>
2018-01-11 23:40:19 -0500

    changed input fields to sliders where possible

--- frontend/js/style_mockup.js
+++ frontend/js/style_mockup.js
@@ -2,3 +2,11 @@
 $(document).on('click', 'div .dropdown-menu', function (e) {
     e.stopPropagation();
 });
+
+// TODO get rid of this, just for testing purpsoes
+setInterval(getTheValue, 500);
+function getTheValue() {
+
+    var mySlider = $("#panelTitleHeight").bootstrapSlider();
+    console.log(mySlider.bootstrapSlider('getValue'))
+}
