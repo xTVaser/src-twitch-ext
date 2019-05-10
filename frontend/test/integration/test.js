e902f456aab7e0edf423d75a80b9eabc54290955
Tyler Wilding <xtvaser@gmail.com>
2019-05-09 22:09:07 -0400

    First simple integration test working

--- frontend/test/integration/test.js
+++ frontend/test/integration/test.js
@@ -1,20 +1,36 @@
-
+var assert = require('assert');
 // Start with a webdriver instance:
+// TODO - also test chrome!
 var sw = require('selenium-webdriver');
 var driver = new sw.Builder()
   .withCapabilities(sw.Capabilities.firefox())
   .build()
 
-var configDriver = new sw.Builder()
-  .withCapabilities(sw.Capabilities.firefox())
-  .build()
+sw.promise.USE_PROMISE_MANAGER = false;
 
-// And then...
 var chai = require('chai');
 var chaiWebdriver = require('chai-webdriver');
 chai.use(chaiWebdriver(driver));
 
-// And you're good to go!
-driver.get('http://stackoverflow.com');
-chai.expect('#mainbar h1').dom.to.contain.text("Top Questions");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('my suite', function() {
+  before("Wait for Browser", async function() {
+    await driver.get('http://localhost:8080/config');
+  });
+
+  // Tests
+  it('Modify Panel Title', async function() {
+    this.timeout(10000);
+    await driver.findElement({xpath: '//*[@data-test="panelTitle"]'}).sendKeys("Test Title");
+    await chai.expect('[data-test=panelTitle]').dom.to.have.value("Test Title");
+  });
+
+  after("Quit", async function() {
+    await driver.quit();
+  });
+});
+
 
