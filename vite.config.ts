78481ab807ea39666b06105d38e7cdbeb0a9cd04
Tyler Wilding <xtvaser@gmail.com>
2023-08-29 18:23:55 -0400

    v2: add CI

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { fileURLToPath } from "url";
 import { resolve } from "path";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-import { normalizePath } from "vite";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
@@ -13,23 +11,7 @@ const outDir = resolve(__dirname, "dist");
 export default defineConfig({
   base: "./",
   root,
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: normalizePath(
-            resolve(
-              __dirname,
-              "node_modules/@shoelace-style/shoelace/dist/assets/",
-            ),
-          ),
-          dest: "/dist/shoelace/",
-        },
-      ],
-    }),
-    basicSsl(),
-    svelte(),
-  ],
+  plugins: [basicSsl(), svelte()],
   resolve: {
     alias: {
       "@components": fileURLToPath(
