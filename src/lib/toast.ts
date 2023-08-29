679ad686da925477536201e4efbe914b705e9aa7
Tyler Wilding <xtvaser@gmail.com>
2023-08-28 22:46:11 -0400

    v2: vendor toast icons

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,6 +1,8 @@
 import type { SlAlert } from "@shoelace-style/shoelace";
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+import exclamationOctagon from "./img/exclamation-octagon.svg";
+import check2Circle from "./img/check2-circle.svg";
 
 // Always escape HTML for text arguments!
 function escapeHtml(html: string) {
@@ -13,7 +15,7 @@ function escapeHtml(html: string) {
 export function notify(
   message: string,
   variant: SlAlert["variant"],
-  icon = "info-circle",
+  icon = "check2-circle",
   duration = 3000,
 ) {
   const alert = document.createElement("sl-alert");
@@ -24,6 +26,11 @@ export function notify(
   const iconElement = document.createElement("sl-icon");
   iconElement.name = icon;
   iconElement.slot = "icon";
+  if (icon === "exclamation-octagon") {
+    iconElement.src = exclamationOctagon;
+  } else if (icon === "check2-circle") {
+    iconElement.src = check2Circle;
+  }
 
   const messageTextNode = document.createTextNode(escapeHtml(message));
 
